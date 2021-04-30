87eae9c49c0968a26d14d684e7a486d6715727fc
Luna <lunifer2004@gmail.com>
2021-04-30 16:29:32 -0500

    FEAT: añado carpeta de la actividad de los taquitos

---
+++ Act_11 Tacos/formTacos.php
@@ -0,0 +1,15 @@
+<?php
+    //---------------------------------------------------------------------------------------------------------------//
+    $Preg1 = (isset($_POST["Preg1"]) && $_POST["Preg1"] != "" )?$_POST["Preg1"] : "No hay dato";
+    $Preg2 = (isset($_POST["Preg2"]) && $_POST["Preg2"] != "" )?$_POST["Preg2"] : "No hay dato";
+    $Preg3 = (isset($_POST["Preg3"]) && $_POST["Preg3"] != "" )?$_POST["Preg3"] : "No hay dato";
+    $Preg4 = (isset($_POST["Preg4"]) && $_POST["Preg4"] != "" )?$_POST["Preg4"] : "No hay dato";
+    $Preg5 = (isset($_POST["Preg5"]) && $_POST["Preg5"] != "" )?$_POST["Preg5"] : "No hay dato";
+    $Preg6 = (isset($_POST["Preg6"]) && $_POST["Preg6"] != "" )?$_POST["Preg6"] : "No hay dato";
+    $Preg7 = (isset($_POST["Preg7"]) && $_POST["Preg7"] != "" )?$_POST["Preg7"] : "No hay dato";
+    $Preg8 = (isset($_POST["Preg8"]) && $_POST["Preg8"] != "" )?$_POST["Preg8"] : "No hay dato";
+    $Preg9 = (isset($_POST["Preg9"]) && $_POST["Preg9"] != "" )?$_POST["Preg9"] : "No hay dato";
+    $Preg10 = (isset($_POST["Preg10"]) && $_POST["Preg10"] != "" )?$_POST["Preg10"] : "No hay dato";
+    //---------------------------------------------------------------------------------------------------------------//
+
+?>
